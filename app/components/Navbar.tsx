c62fa7b13a22b788a8c5828b308be19a6a23b9f8
Ombre <oliverthek@gmail.com>
2024-12-01 17:40:09 +0100

    start

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,6 +2,14 @@ import Link from "next/link";
 import React from "react";
 import MobileMenu from "./MobileMenu";
 import Image from "next/image";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
@@ -49,7 +57,37 @@ const Navbar = () => {
         </div>
       </div>
       {/* RIGHT */}
-      <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
+      <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end text-white">
+        <ClerkLoading>
+          <div
+            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+            role="status"
+          ></div>
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <div className="cursor-pointer">
+              <Image
+                src="/messages.png"
+                alt="messages"
+                width={20}
+                height={20}
+              ></Image>
+            </div>
+            <div className="cursor-pointer">
+              <Image
+                src="/notifications.png"
+                alt="notifications"
+                width={20}
+                height={20}
+              ></Image>
+            </div>
+            <UserButton></UserButton>
+          </SignedIn>
+          <SignedOut>
+            <Link href={"/sign-in"}>Sign in</Link>
+          </SignedOut>
+        </ClerkLoaded>
         <MobileMenu />
       </div>
     </div>
