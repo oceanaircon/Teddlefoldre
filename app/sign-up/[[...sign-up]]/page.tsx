62c22182a3c9c809e5f9a4075984e68802880c3e
Ombre <oliverthek@gmail.com>
2024-12-19 13:09:46 +0100

    width setting

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="h-[calc(100vh-96px)] flex items-center justify-center">
+    <div className="h-64 flex items-center justify-center">
       <SignUp />;
     </div>
   );
